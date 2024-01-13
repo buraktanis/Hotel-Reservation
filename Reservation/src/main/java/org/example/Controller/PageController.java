abfe6086fed3a6ddda42443b195880833535ba01
BurakTanis <burak.tanis@std.yeditepe.edu.tr>
2024-01-13 23:20:29 +0300

    reservation service done.

--- Reservation/src/main/java/org/example/Controller/PageController.java
+++ Reservation/src/main/java/org/example/Controller/PageController.java
@@ -2,8 +2,10 @@ package org.example.Controller;
 import org.example.Service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -13,9 +15,10 @@ public class PageController {
     private ReservationService reservationService;
 
     @GetMapping("/reservation")
-    public String showReservationPage() {
-
+    public String showReservationPage(@RequestParam(name = "roomNumber", required = false) Integer roomNumber, Model model) {
+        if (roomNumber != null) {
+            model.addAttribute("roomNumber", roomNumber);
+        }
         return "reservation";
     }
-
 }
--- Reservation/src/main/java/org/example/Entity/Reservations.java
+++ Reservation/src/main/java/org/example/Entity/Reservations.java
@@ -8,6 +8,17 @@ public class Reservations {
     private Integer room;
     private Date startdate;
     private Date enddate;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 
     public String getUser() {
         return user;
